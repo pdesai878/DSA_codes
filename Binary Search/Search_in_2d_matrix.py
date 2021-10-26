3a92d50dc2e5d5861c0a8685e95b9cc7daf462e8
pdesai878 <63565627+pdesai878@users.noreply.github.com>
2021-10-26 23:05:35 +0530

    added search in 2d matrix

---
+++ Binary Search/Search_in_2d_matrix.py
@@ -0,0 +1,29 @@
+"""
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+"""
+
+#TC: LOG(M*N)
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or target is None:
+            return False
+
+        rows, cols = len(matrix), len(matrix[0])
+        low, high = 0, rows * cols - 1
+        
+        while low <= high:
+            mid = (low + high)//2
+            num = matrix[mid//cols][mid%cols]
+
+            if num == target:
+                return True
+            elif num < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return False
