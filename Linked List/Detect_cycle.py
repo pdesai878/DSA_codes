358119b57705471f4fdedd9c05bf56c5789e3450
pdesai878 <63565627+pdesai878@users.noreply.github.com>
2021-10-27 23:12:18 +0530

    added detect cycle in a linked list

---
+++ Linked List/Detect_cycle.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        nodes=set()
+        while head:
+            if head in nodes:
+                return True
+            nodes.add(head)
+            head=head.next 
+        return False
+        
+        
+****************************************************************
+# O(1) space
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head is None:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
